5fc82d00cda0ca04cbafc810777c9ac83bb37919
xastraxxx <155238640+xastraxxx@users.noreply.github.com>
2024-09-07 16:50:13 +0200

    Update MarteNFT.sol

--- MarteNFT.sol
+++ MarteNFT.sol
@@ -3,19 +3,93 @@
 // Compatible with OpenZeppelin Contracts ^5.0.0
 pragma solidity ^0.8.20;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721EnumerableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721URIStorageUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721PausableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721BurnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/utils/cryptography/EIP712Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721VotesUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 
-contract MarteNFT is ERC20, ERC20Burnable, Ownable {
-    constructor(address initialOwner)
-        ERC20("MarteNFT", "Mnft")
-        Ownable(initialOwner)
-    {
-        _mint(msg.sender, 10 * 10 ** decimals());
+contract MarteNFT is Initializable, ERC721Upgradeable, ERC721EnumerableUpgradeable, ERC721URIStorageUpgradeable, ERC721PausableUpgradeable, OwnableUpgradeable, ERC721BurnableUpgradeable, EIP712Upgradeable, ERC721VotesUpgradeable, UUPSUpgradeable {
+    /// @custom:oz-upgrades-unsafe-allow constructor
+    constructor() {
+        _disableInitializers();
     }
 
-    function mint(address to, uint256 amount) public onlyOwner {
-        _mint(to, amount);
+    function initialize(address initialOwner) initializer public {
+        __ERC721_init("MarteNFT", "mNFT");
+        __ERC721Enumerable_init();
+        __ERC721URIStorage_init();
+        __ERC721Pausable_init();
+        __Ownable_init(initialOwner);
+        __ERC721Burnable_init();
+        __EIP712_init("MarteNFT", "1");
+        __ERC721Votes_init();
+        __UUPSUpgradeable_init();
+    }
+
+    function _baseURI() internal pure override returns (string memory) {
+        return "https://www.google.com/search?sca_esv=47e9e4b0031c9f45&sca_upv=1&sxsrf=ADLYWIItVN9ofCB_ZLSvsTVEPPv3LLgZbg:1725720569394&q=rick+rolling&udm=2&fbs=AEQNm0AbzhUJjXv6jRup8eVc0BvPyH5PazCaW205cG-Bd0in0dvSnU368ct8hcGzY9zRCgaFuOXcgHiRWQ-LaW5vxLOfE8kPZbIkxC0P67NhBVF3FGtrD6AFhLGkxGjCmk1D0Ryo7I79Mv4sM88RpJ0T1pFV8EiwZX0hZ93dCcTJG4tO3rReVJn5zlD97imzxC0ZzBWbzXB_h63YWfMgkjNUz4BTnjVKew&sa=X&ved=2ahUKEwjcuJSMirGIAxWiDDQIHTryAJAQtKgLegQIFBAB&biw=1640&bih=1147&dpr=1.35#vhid=NxSdSwC6Lb-GWM&vssid=mosaic";
+    }
+
+    function pause() public onlyOwner {
+        _pause();
+    }
+
+    function unpause() public onlyOwner {
+        _unpause();
+    }
+
+    function safeMint(address to, uint256 tokenId, string memory uri)
+        public
+        onlyOwner
+    {
+        _safeMint(to, tokenId);
+        _setTokenURI(tokenId, uri);
+    }
+
+    function _authorizeUpgrade(address newImplementation)
+        internal
+        onlyOwner
+        override
+    {}
+
+    // The following functions are overrides required by Solidity.
+
+    function _update(address to, uint256 tokenId, address auth)
+        internal
+        override(ERC721Upgradeable, ERC721EnumerableUpgradeable, ERC721PausableUpgradeable, ERC721VotesUpgradeable)
+        returns (address)
+    {
+        return super._update(to, tokenId, auth);
+    }
+
+    function _increaseBalance(address account, uint128 value)
+        internal
+        override(ERC721Upgradeable, ERC721EnumerableUpgradeable, ERC721VotesUpgradeable)
+    {
+        super._increaseBalance(account, value);
+    }
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721Upgradeable, ERC721URIStorageUpgradeable)
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721Upgradeable, ERC721EnumerableUpgradeable, ERC721URIStorageUpgradeable)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
     }
 }
